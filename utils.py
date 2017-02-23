509192095267db709213673e4757fefc15dfbf63
Richard Milne <richmilne@hotmail.com>
2017-02-23 16:41:08 +0100

    Re-wrote parse and display funcs, and docstrings, to take user-defined puzzle size into account.

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import os
+import re
 import sys
 import string
+
+ws = '\n\r\t |+-'
+re_ws = re.compile(r"""(\n|\r|\t| |\||\+|-)""")
+
 
 def cross(a, b):
     return [s+t for s in a for t in b]
@@ -18,6 +23,9 @@ def init(side, wildcard='.', diagonal=False):
     cols = '1234567890' + string.lowercase
     rows, cols = [seq[:dim] for seq in [rows, cols]]
     symbols = cols
+
+    for c in ws + wildcard:
+        assert c not in symbols
 
     sub_slices = []
     for i in xrange(side):
@@ -52,7 +60,7 @@ def init(side, wildcard='.', diagonal=False):
     peer_tups = [(s, sorted(set(flatten(units[s])) - set([s]))) for s in boxes]
     peers = dict(((s, set(peers)) for s, peers in peer_tups))
 
-    if 1:
+    if 0:
         val_list = [('rows', row_units),
                     ('cols', col_units),
                     ('blocks', square_units),
@@ -67,34 +75,58 @@ def init(side, wildcard='.', diagonal=False):
                 print line
 
 
-def display(values):
-    """
-    Display the values as a 2-D grid.
-    Input: The sudoku in dictionary form
-    Output: None
-    """
-    width = 1+max(len(values[s]) for s in boxes)
-    line = '+'.join(['-'*(width*3)]*3)
-    for r in rows:
-        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
-                      for c in cols))
-        if r in 'CF': print(line)
-    print
+    def display(values):
+        """
+        Display the values as a 2-D grid.
+        Input: The sudoku in dictionary form
+        Output: None
+        """
+        add_sep = lambda idx: 0 < idx < (dim-1) and (idx % side) == (side-1)
 
-def grid_values(grid):
-    """
-    Convert grid into a dict of {square: char} with '123456789' for empties.
-    Input: A grid in string form.
-    Output: A grid in dictionary form
-            Keys: The boxes, e.g., 'A1'
-            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
-    """
-    chars = []
-    digits = '123456789'
-    for c in grid:
-        if c in digits:
-            chars.append(c)
-        if c == '.':
-            chars.append(digits)
-    assert len(chars) == 81
-    return dict(zip(boxes, chars))
+        width = 1 + max(len(v) for v in values.values())
+        line = '+'.join(['-'*(width*side)]*side)
+        for r, row in enumerate(rows):
+            disp = [values[row+col].center(width)+('|' if add_sep(c) else '')
+                    for c, col in enumerate(cols)]
+            print(''.join(disp))
+            if add_sep(r): print(line)
+        print
+    
+    def parse_grid(grid, wildcard=wildcard):
+        """Parse the string representation of a Sudoku grid.
+
+        The input string is split on each of the whitespace or table formatting
+        chars given at the top of this module, and contents of each cell in
+        the puzzle are returned in a list.
+        """
+        grid = re_ws.sub(' ', grid)
+        puzzle = [c.strip() for c in grid.split(' ') if c.strip()]
+        new = []
+        for piece in puzzle:
+            if wildcard in piece and len(piece) > 1:
+                new.extend([c for c in piece])
+            else:
+                new.append(piece)
+        return new
+
+    def grid_values(grid, wildcard=wildcard):
+        """
+        Convert grid into a dict of {square: char} with
+        '%(symbols)s' for empties (wildcard characters.)
+        Input: A grid in string form.
+        Output: A grid in dictionary form
+                Keys: The boxes, e.g., 'A1'
+                Values: The value in each box, e.g., '8'. If the box has no
+                value, then the value will be '%(symbols)s'.
+        """
+        puzzle = parse_grid(grid, wildcard)
+        if len(puzzle) != length:
+            msg = 'Input grid must be a string of length %d (%dx%d)'
+            raise AssertionError(msg % (length, dim, dim))
+        puzzle = [symbols if c==wildcard else c for c in puzzle]
+        return dict(zip(boxes, puzzle))
+    grid_values.__doc__ %= locals()
+
+    return display, parse_grid, grid_values
+
+display, parse_grid, grid_values = init(3)
